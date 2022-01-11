5ad3bfd4627b44bfeee604e2c4182e8b7d7bfc33
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-11 22:35:16 +0300

    Add project files.

---
+++ WebApplication3/Entitiy/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Entitiy
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
