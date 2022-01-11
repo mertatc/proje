954ccddf09a3a0ef89f971e62c443526a775c0c6
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-11 23:05:47 +0300

    Upd

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication3.Entitiy;
 
 namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext context = new DataContext();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
     }
 }
