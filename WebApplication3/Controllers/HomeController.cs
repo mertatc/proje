d2d2cfb74c9ee84ab88beb6ce617703194c7e9b7
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-15 21:45:48 +0300

    Upd

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -38,11 +38,11 @@ namespace WebApplication3.Controllers
             var urunler = _context.Products.Where(i => i.IsApproved).Select(i => new ProductModel()
             {
                 Id = i.Id,
-                Name = i.Name,
+                Name = i.Name.Length > 50 ? i.Name.Substring(0, 47) + "..." : i.Name,
                 Description = i.Description.Length > 50 ? i.Description.Substring(0, 47) + "..." : i.Description,
                 Price = i.Price,
                 Stock = i.Stock,
-                //Image = i.Image==null?"1.jpg":i.Image , 
+                //Image = i.Image==null?"1.jpg":i.Image ,          //OLMAYAN RESİMLERE 1.JPG ATANIR
                 CategoryId = i.CategoryId
 
 
