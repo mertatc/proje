98cc7bda3585043a25f2859439c3a41b24f5dc28
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-26 15:46:04 +0300

    Upd

---
+++ WebApplication3/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Identity;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<ApplicationRole> RoleManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<ApplicationRole>(roleStore);
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //kayıt işlemleri
+                var user = new ApplicationUser();   
+                user.Name = model.Name;
+                user.Surname = model.SurName;
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+
+                IdentityResult result = UserManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    //kullanıcı oluştu ve kullanıcıyı bir role atayabilrisniz
+                    //RoleManager.AddToRole(user.Id, "User");
+                }
+
+            }
+            return View();
+        }
+    }
+}
