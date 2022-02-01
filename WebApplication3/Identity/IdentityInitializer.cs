cf9497a7a776df4949dc69ac594d20b23d56ba35
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-02-02 02:04:39 +0300

    Upd

--- WebApplication3/Identity/IdentityInitializer.cs
+++ WebApplication3/Identity/IdentityInitializer.cs
@@ -19,8 +19,28 @@ namespace WebApplication3.Identity
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-            }
+                var role = new ApplicationRole("admin", "yönetici");
 
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(i => i.Name == "user"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new ApplicationRole("user", "user rolü");
+
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(i => i.Name == "mert"))
+            {
+                var store = new UserStore<IdentityUser>(context);
+                var manager = new UserManager<IdentityUser>(store);
+                var user = new ApplicationUser() { Name = "mert", Surname = "atc", UserName = "mertatc", Email = "mertatc@hotmail.com" };
+
+                manager.Create(user,"1234567");
+                manager.AddToRole(user.Id, "admin");
+                manager.AddToRole(user.Id, "user");
+            }
 
             //user
             base.Seed(context);
